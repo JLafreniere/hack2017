379c1f37cb6f0d97694efe45a1a3271fd905ddae
JLafreniere <jonathan.lafreniere@hotmail.com>
2017-01-28 19:54:41 -0500

    Initialisation du repertoire

---
+++ Synchroniser.bat
@@ -0,0 +1,13 @@
+@echo off
+pushd %~dp0
+git add .
+set /p message="Message du commit: "
+git commit -m "%message%"
+git pull
+git push
+echo.   
+echo     --------------------
+echo     FIN DE LA SAUVEGARDE
+echo     --------------------
+echo.   
+pause
